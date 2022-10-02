c5e5c44b4a5306197d7a4b6bb8bfe72a7d4e025a
Stanislav <9323354@gmail.com>
2022-10-02 14:59:54 +0300

    Completed Task 1

--- HomeWork02_10_2022/Program.cs
+++ HomeWork02_10_2022/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"Число {A} в степени {B} равно {Degree(A)}");
+
+
+int Degree (int first)
+{
+    int degree = Convert.ToInt32(Math.Pow(A, B));
+    return degree;
+}
