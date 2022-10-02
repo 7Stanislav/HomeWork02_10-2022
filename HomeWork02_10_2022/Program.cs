3d62060ca212cd7b9c58c2cdd810d95217b03cfd
Stanislav <9323354@gmail.com>
2022-10-02 16:43:38 +0300

    Completed Task 2

--- HomeWork02_10_2022/Program.cs
+++ HomeWork02_10_2022/Program.cs
@@ -2,6 +2,7 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+/*
 Console.WriteLine("Введите число А: ");
 int A = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число B: ");
@@ -13,4 +14,27 @@ int Degree (int first)
 {
     int degree = Convert.ToInt32(Math.Pow(A, B));
     return degree;
+}
+*/
+
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+string arr = A.ToString();
+Console.WriteLine($"Сумма цифр в числе равна {GetSum(A)}");
+
+int GetSum(int limit)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum = sum + A % 10;
+        A = A / 10;
+    }
+    return sum;
 }
